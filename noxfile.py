d62d8597718a03bfe753c105519d2f1d3ed33f22
choldgraf <choldgraf@gmail.com>
2026-03-16 13:08:24 -0700

    Move to docs/ folder

--- noxfile.py
+++ noxfile.py
@@ -9,12 +9,21 @@ def test(session):
 
 
 @nox.session
+def build(session):
+    """Build the plugin bundle."""
+    session.run("npm", "install", external=True)
+    session.run("npm", "run", "build", external=True)
+
+
+@nox.session
 def docs(session):
     """Build the documentation."""
-    session.run("npx", "mystmd", "build", external=True)
+    with session.chdir("docs"):
+        session.run("npx", "mystmd", "build", "--html", external=True)
 
 
 @nox.session(name="docs-live")
 def docs_live(session):
     """Start a live development server."""
-    session.run("npx", "mystmd", "start", external=True)
+    with session.chdir("docs"):
+        session.run("npx", "mystmd", "start", external=True)
